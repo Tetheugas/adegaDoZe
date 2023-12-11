eb9d6aa408aa5f3eb97d3ed1d699fbf33ae2984f
Matheus Silva <98681828+Tetheugas@users.noreply.github.com>
2023-12-11 02:53:21 -0300

    Initial commit

---
+++ src/main/java/com/example/adegaDoZe/AdegaDoZeApplication.java
@@ -0,0 +1,13 @@
+package com.example.adegaDoZe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdegaDoZeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdegaDoZeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adegaDoZe/AdegaDoZeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adegaDoZe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdegaDoZeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
